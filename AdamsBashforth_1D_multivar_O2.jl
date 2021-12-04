22c7c1b93e78041719ef728f0374717a7171c504
Michael Bell <mmbell@colostate.edu>
2021-12-04 09:23:02 -0700

    Working Williams et al 2013 TCBL model

--- AdamsBashforth_1D_multivar_O2.jl
+++ AdamsBashforth_1D_multivar_O2.jl
@@ -80,53 +80,112 @@ function calcTendency(splines::Vector{Spline1D}, model::ModelParameters, t::int)
     end
     
     # Feed physical matrices to physical equations
-    udot, F = physical_model(model,u,ux,uxx)
+    udot, F = physical_model(model,splines[1].mishPoints,u,ux,uxx)
 
     for v in eachindex(splines)
-        # Do something with F later. May need to be a new spline because of BCs?
-        # SBxtransform(spline,F,BCL,BCR)
-        bdot_delay[:,v] = zeros(real,splines[v].bDim)
-
         bdot[:,v] = SBtransform(splines[v],udot[:,v])
+        bdot_delay[:,v] = zeros(real,splines[v].bDim)
     end
+    
+    # Do something with F. May need to be a new spline because of BCs?
+    Fspline = Spline1D(SplineParameters(xmin = model.xmin, xmax = model.xmax,
+            num_nodes = model.num_nodes, BCL = R0, BCR = R0))
+    # SBxtransform(spline,F,BCL,BCR) not working
+    Fspline.uMish .= F[:,2]
+    SBtransform!(Fspline)
+    SAtransform!(Fspline)
+    bdot_delay[:,2] = SBtransform(Fspline,SIxtransform(Fspline))
+    Fspline.uMish .= F[:,3]
+    SBtransform!(Fspline)
+    SAtransform!(Fspline)
+    bdot_delay[:,3] = SBtransform(Fspline,SIxtransform(Fspline))
+   
     return bdot,bdot_delay
 end
 
-function physical_model(model::ModelParameters,u::Matrix{real},ux::Matrix{real},uxx::Matrix{real})
+function physical_model(model::ModelParameters,x::Vector{real},var::Matrix{real},varx::Matrix{real},varxx::Matrix{real})
     
-    udot = zeros(real,size(u, 1),size(u, 2))
-    F = zeros(real,size(u, 1),size(u, 2))
+    vardot = zeros(real,size(var, 1),size(var, 2))
+    F = zeros(real,size(var, 1),size(var, 2))
     if model.equation_set == "1dLinearAdvection"
         #1D Linear advection to test
         c_0 = 1.0
         K = 0.003
         
-        udot[:,1] = -c_0 .* ux[:,1] + (K .* uxx[:,1])
-        udot[:,2] = -c_0 .* ux[:,2] + (K .* uxx[:,2])
+        vardot[:,1] = -c_0 .* varx[:,1] + (K .* varxx[:,1])
+        vardot[:,2] = -c_0 .* varx[:,2] + (K .* varxx[:,2])
         F = 0
     elseif model.equation_set == "1dNonlinearAdvection"
         c_0 = 1.0
         K = 0.048
         
-        udot[:,1] = -(c_0 .+ u[:,1]) .* ux[:,1] + (K .* uxx[:,1])
+        udot[:,1] = -(c_0 .+ var[:,1]) .* varx[:,1] + (K .* varxx[:,1])
         F = 0
     elseif model.equation_set == "1dLinearShallowWater"
         K = 0.003
         g = 9.81
         H = 1.0
         
-        udot[:,1] = -g .* ux[:,2]
-        udot[:,2] = -H .* ux[:,1]
+        vardot[:,1] = -g .* varx[:,2]
+        vardot[:,2] = -H .* varx[:,1]
         F = 0
+    elseif model.equation_set == "Williams2013_TCBL"
+        
+        #vardot, F = Williams2013_TBCL(model,x,var,varx,varxx)
+        K = 1500.0
+        Cd = 2.4e-3
+        h = 1000.0
+        f = 5.0e-5
+
+        vgrdot = zeros(real,length(x))
+        F = zeros(real,length(x),4)
+
+        vgr = var[:,1]
+        u = var[:,2]
+        v = var[:,3]
+        r = x
+        U = 0.78 * sqrt.((u .* u) .+ (v .* v))
+
+        ur = varx[:,2]
+        vr = varx[:,3]
+
+        urr = varxx[:,2]
+        vrr = varxx[:,3]
+
+        w = -h .* ((u ./ r) .+ ur)
+        w_ = 0.5 .* abs.(w) .- w
+
+        vardot[:,1] .= vgrdot
+        
+        UADV = -(u .* ur)
+        UDRAG = -(Cd .* U .* u ./ h)
+        UCOR = ((f .* v) .+ ((v .* v) ./ r))
+        UPGF = -((f .* vgr) .+ ((vgr .* vgr) ./ r))
+        UW = -(w_ .* (u ./ h))
+        UKDIFF = K .* ((u ./ r) .+ ur)
+        vardot[:,2] .= UADV .+ UDRAG .+ UCOR .+ UPGF .+ UW
+        F[:,2] .= UKDIFF
+
+        
+        VADV = -u .* (f .+ (v ./ r) .+ vr)
+        VDRAG = -(Cd .* U .* v ./ h)
+        VW = w_ .* (vgr - v) ./ h
+        VKDIFF = K .* ((v ./ r) .+ vr)
+        vardot[:,3] .= VADV .+ VDRAG .+ UW
+        F[:,3] .= VKDIFF
+        
+        vardot[:,4] .= w
+
     else
-        throw(MethodError("Selected equation set not implemented"))
+        error("Selected equation set not implemented")
     end
     
-    return udot, F 
+    return vardot, F 
 end
 
 function write_output(splines::Vector{Spline1D}, model::ModelParameters, t::int)
     
+    println("Writing output at time $t")
     for var in keys(model.vars)
         v = model.vars[var]
         afilename = string("model_a_", var , "_", t, ".csv")
@@ -167,9 +226,18 @@ function initialize(model::ModelParameters)
     #end
     
     # Hard-code IC for testing
+    V0 = 50.0 / 20000.0
     for i = 1:splines[1].mishDim
         splines[model.vars["u"]].uMish[i] = 0
-        splines[model.vars["h"]].uMish[i] = exp(-(splines[1].mishPoints[i])^2 / (2 * 4^2))
+        #splines[model.vars["h"]].uMish[i] = exp(-(splines[1].mishPoints[i])^2 / (2 * 4^2))
+        if (splines[1].mishPoints[i] < 20000.0)
+            splines[model.vars["vgr"]].uMish[i] = V0 * splines[1].mishPoints[i]
+            splines[model.vars["v"]].uMish[i] = V0 * splines[1].mishPoints[i]
+        else
+            splines[model.vars["vgr"]].uMish[i] = 4.0e8 * V0 / (splines[1].mishPoints[i])
+            splines[model.vars["v"]].uMish[i] = 4.0e8 * V0 / (splines[1].mishPoints[i])
+        end
+        splines[model.vars["w"]].uMish[i] = 0
     end
     #setMishValues(spline,ic.u)
 
@@ -191,7 +259,7 @@ function run(splines::Vector{Spline1D}, model::ModelParameters)
     # Advance the first timestep
     bdot,bdot_delay = calcTendency(splines, model, 1)
     b_nxt, bdot_svd = first_timestep(splines, bdot, model.ts)
-    for v in eachindex(splines)
+    for v in Base.OneTo(length(splines)-1)
         splines[v].b .= b_nxt[:,v]
         SAtransform!(splines[v])
         SItransform!(splines[v])
@@ -200,12 +268,17 @@ function run(splines::Vector{Spline1D}, model::ModelParameters)
     # Keep going!
     for t = 2:model.num_ts
         bdot,bdot_delay = calcTendency(splines, model, t)
+                
         b_nxt, bdot_svd = timestep(splines, bdot, bdot_svd, bdot_delay, model.ts)
-        for v in eachindex(splines)
+        for v in Base.OneTo(length(splines)-1)
             splines[v].b .= b_nxt[:,v]
             SAtransform!(splines[v])
             SItransform!(splines[v])
         end
+        # Override diagnostic variables
+        splines[4].b .= bdot[:,4]
+        SAtransform!(splines[4])
+        SItransform!(splines[4])
     end
     
     println("Done with time integration")
@@ -222,22 +295,60 @@ end
 function integrate_model()
     
     model = ModelParameters(
-        ts = 0.1,
-        num_ts = 480,
-        output_interval = 25,
-        xmin = -48.0,
-        xmax = 48.0,
-        num_nodes = 96,
-        BCL = Dict("u" => R2T20, "h" => R1T1),
-        BCR = Dict("u" => R0, "h" => R0),
-        equation_set = "1dLinearShallowWater",
+        ts = 1.0,
+        num_ts = 10800,
+        output_interval = 3600,
+        xmin = 0.0,
+        xmax = 1.0e6,
+        num_nodes = 2000,
+        BCL = Dict("vgr" => R0, "u" => R1T0, "v" => R1T0, "w" => R1T0),
+        BCR = Dict("vgr" => R0, "u" => R1T1, "v" => R1T1, "w" => R1T1),
+        equation_set = "Williams2013_TCBL",
         initial_conditions = "testcase.csv",
-        vars = Dict("u" => 1, "h" => 2)    
+        vars = Dict("vgr" => 1, "u" => 2, "v" => 3, "w" => 4)    
     )
    
     splines = initialize(model)
     splines = run(splines, model)
     finalize(splines, model)
+end
+
+function Williams2013_TBCL(model::ModelParameters,x::Vector{real},
+        var::Matrix{real},varx::Matrix{real},varxx::Matrix{real})
+
+    K = 1500.0
+    Cd = 2.4e-3
+    h = 1000.0
+    f = 5.0e-5
+
+    udot = zeros(real,length(x))
+    vdot = zeros(real,length(x))
+    vgrdot = zeros(real,length(x))
+    F = zeros(real,length(x),4)
+    
+    vgr = var[:,model.vars["vgr"]]
+    u = var[:,model.vars["u"]]
+    v = var[:,model.vars["v"]]
+    r = x
+    U = 0.78 * sqrt.(u .* u + v .* v)
+    
+    ur = varx[:,model.vars["u"]]
+    vr = varx[:,model.vars["v"]]
+    
+    urr = varxx[:,model.vars["u"]]
+    vrr = varxx[:,model.vars["v"]]
+    
+    w = -h * ((u ./ r) .+ ur)
+    w_ = 0.5 * abs.(w) - w
+    
+    udot = (-u .* ur) .- (w_ .* (u ./ h)) .+ (f .+ (v .+ vgr) ./ r) .* (v .- vgr) 
+        .- (Cd .* U .* u ./ h) .+ K .* ((ur ./ r) .+ (u ./ (r .* r)) .+ urr)
+        
+    vdot = (w_ .* (vgr - v) ./ h) .- (f .+ (v ./ r) .+ vr) .* u
+        .- (Cd .* U .* v ./ h) .+ K .* ((vr ./ r) .+ (v ./ (r .* r)) .+ vrr)
+    
+    tendency = [vgrdot udot vdot w]
+    return tendency, F
 end
 
 end
