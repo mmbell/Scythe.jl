ac7736c1625fb519a9d84d0aa9307cbb95684244
Michael Bell <mmbell@colostate.edu>
2025-03-20 08:39:34 -0600

    Add preliminary support for rain microphysics

--- src/microphysics.jl
+++ src/microphysics.jl
@@ -92,13 +92,13 @@ function q_condensation(qss, Tk, p, q_v, q_l, N_c, r_c)
     return q_cond*invtau
 end
 
+# Here
 function s_condensation(q_cond, Tk, rho_d, q_v, q_l, p)
 
     Cm = (q_l * Cl)/(Cvd + (q_v * Cvv) + (q_l * Cl))
     #ds = q_cond * ((L_v(Tk)*(1.0 - Cm)/Tk) - Scythe.vapor_entropy(Tk, rho_d, q_v) + Rv)
     e = vapor_pressure(p, q_v)
     sat_e = sat_pressure_liquid_buck(Tk, p)
-
     ds = q_cond * ( ((-L_v(Tk)* Cm)/Tk) -(Cl * log(Tk / T_0)) + (Rv*log(e/sat_e)) )
     return ds
 end
@@ -152,8 +152,11 @@ function condensation_adjustment(mtile::ModelTile, colstart::Int64, colend::Int6
     qv_n = view(mtile.impdot_n,colstart:colend,mu_index)
     qv_nm1 = view(mtile.impdot_nm1,colstart:colend,mu_index)
 
-    mu_l_index = mtile.model.grid_params.vars["mu_l"]
-    mu_l = view(mtile.var_np1,colstart:colend,mu_l_index)
+    mu_c_index = mtile.model.grid_params.vars["mu_c"]
+    mu_c = view(mtile.var_np1,colstart:colend,mu_c_index)
+
+    mu_r_index = mtile.model.grid_params.vars["mu_r"]
+    mu_r = view(mtile.var_np1,colstart:colend,mu_r_index)
 
     qss_index = mtile.model.grid_params.vars["qss"]
     qss = view(mtile.var_np1,colstart:colend,qss_index)
@@ -164,34 +167,95 @@ function condensation_adjustment(mtile::ModelTile, colstart::Int64, colend::Int6
     s_total = s .+ mtile.ref_state.sbar[:,1]
     xi_total = xi .+ mtile.ref_state.xibar[:,1]
     mu_total = mu .+ mtile.ref_state.mubar[:,1]
-    mu_l_total = mu_l .+ mtile.ref_state.mu_lbar[:,1]
 
     thermo = thermodynamic_tuple.(s_total, xi_total, mu_total)
     q_v = [x[1] for x in thermo]    # Total water vapor mixing ratio
     rho_d = [x[2] for x in thermo]  # Dry air density
     Tk = [x[3] for x in thermo]     # Temperature in K
     p = [x[4] for x in thermo]      # Total air pressure
-    q_l = ahyp.(mu_l_total)         # Liquid mixing ratio
+    q_c = ahyp.(mu_c)               # Condensate mixing ratio
+    q_r = ahyp.(mu_r)               # Precipitation mixing ratio
+    q_l = q_c .+ q_r                # Liquid mixing ratio
     q_sat = q_sat_liquid.(Tk, p)
     Q_s = Q_s_factor.(Tk, p, q_v, q_l)
 
     # Do the increment using explicit Euler integration
     tau_r = 0.25
-    #dq = @. tau_r * ( ((2.0 * Q_s - 1.0) * q_v) - (0.75 * Q_s * qv_n) + q_sat + qss) / (1.0 + (1.25 * Q_s * tau_r))
     q_cond = (q_v .- q_sat .- qss) ./ (1.0 .+ Q_s)
     q_cond = min(q_v, q_cond)
-    q_cond = max(-q_l, q_cond)
+    # Restrict adjustment to condensate and not rain
+    q_cond = max(-q_c, q_cond)
     mu .= @. mu - tau_r * dmudq(mu_total, q_v) * q_cond
-    mu_l .= @. mu_l + tau_r * dmudq(mu_l_total, q_l) * q_cond
+    mu_c .= @. mu_c + tau_r * dmudq(mu_c, q_c) * q_cond
     s .= @. s + tau_r * s_condensation(q_cond, Tk, rho_d, q_v, q_l, p)
 
-    #mu .= @. mu + tau_r * dmudq(mu_total, q_v) * ((1.25 * qss) -(Q_s*qv_n + qss_n) + (0.75 * (Q_s*qv_nm1 + qss_nm1)) - (1.0 + Q_s)*q_v + q_sat) / (1.0 - 0.625*Q_s)
-    #q_v_np1 = ahyp.(mu)
-    #q_dotnp1 = -Q_s .* q_v_np1 .- qss
-    #mu_l .= @. mu_l + tau_r * dmudq(mu_l_total, q_l) * ((1.25 * q_dotnp1) +(Q_s*qv_n + qss_n) - (0.75 * (Q_s*qv_nm1 + qss_nm1)) + (1.0 + Q_s)*q_v - q_sat)
-    #s .= @. s + tau_r * ((L_v(Tk)/Tk) - Scythe.vapor_entropy(Tk, rho_d, q_v) + Rv)*((1.25 * q_dotnp1) +(Q_s*qv_n + qss_n) - (0.75 * (Q_s*qv_nm1 + qss_nm1)) + (1.0 + Q_s)*q_v - q_sat)
+    # Incorrect implicit method
+    #dq = @. tau_r * ( ((2.0 * Q_s - 1.0) * q_v) - (0.75 * Q_s * qv_n) + q_sat + qss) / (1.0 + (1.25 * Q_s * tau_r))
 
-    #qv_nm1 .= qv_n
-    #qss_nm1 .= qss_n
+end
 
+function autoconversion(q_c, rho_d)
+
+    # From Ooyama (2001)
+    q_auto = 0.001*(q_c - 0.001)
+    if q_auto < 0.0
+        q_auto = 0.0
+    end
+    return q_auto
+end
+
+function collection(q_c, q_r, rho_d, Tk)
+
+    # From Ooyama (2001)
+    q_coll = 2.20* q_c * (q_r)^0.875 * f_ice(Tk)
+    if q_coll < 0.0
+        q_coll = 0.0
+    end
+    return q_coll
+end
+
+function f_ice(Tk)
+
+    # From Ooyama (2001)
+    if Tk < 273.15
+        return 0.2 + 0.8 * sech((273.15 - Tk)/5.0)
+    else
+        return 1.0
+    end
+end
+
+function rain_evaporation(q_r, rho_d, Tk, p)
+
+    # From Ooyama (2001)
+    e_s = sat_pressure_liquid_buck(Tk, p)
+    rho_vs = e_s / (Rv * Tk)
+    rho_r = q_r * rho_d
+    q_evap = (f_ventilation(q_r, rho_d, Tk) * rho_r^0.525)/(1.0e4*((2.03 * rho_vs) + (3.337/Tk))) 
+    if q_evap < 0.0
+        q_evap = 0.0
+    end
+    # This is multiplied by qss so the 1/rho_d factor is already included
+    return q_evap
+end
+
+function f_ventilation(q_r, rho_d, Tk)
+
+    # From Ooyama (2001)
+    rho_r = q_r * rho_d
+    f_vent = 1.6 + 30.39 * rho_r^0.2046 * f_ice(Tk)^1.5
+    if f_vent < 0.0
+        f_vent = 0.0
+    end
+    return f_vent
+end
+
+function sedimentation(q_r, rho_d, Tk)
+
+    # From Ooyama (2001)
+    rho_r = q_r * rho_d
+    Vt = -14.164 * rho_r^0.1364 * (rho_d0/rho_d)^0.5 * f_ice(Tk)
+    if Vt < 0.0
+        Vt = 0.0
+    end
+    return Vt
 end
